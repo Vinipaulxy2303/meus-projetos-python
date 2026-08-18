5cce1dbb3d5840bfba3ff355820b389b5309119b
Vinícius de Paulo Garcia <viniciusdepaulogarcia2303@gmail.com>
2026-08-18 00:27:15 +0000

    estamos tentando

--- listagem.py
+++ listagem.py
@@ -1,6 +1,11 @@
-def listagem():
-    alphabet='abcdefghijklmnopqrstuvwxyz'
-    nome=str(input('Qual é o teu nome : '))
-    while i in alphabet():
-        print(i += nome )
-        
+i=0
+def nota():
+    nome=str(input('Qual o teu nome: '))
+    nota1=float(input("Qual foi a sua primeira nota: "))
+    nota2=float(input("Qual foi a sua segunda nota: "))
+    result=(nota1+nota2)/2
+    print(result)
+    return(nota1, nota2, nome, result)
+for i in range(4):
+    nota()
+    if 
